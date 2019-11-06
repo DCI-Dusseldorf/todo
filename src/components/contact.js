1aa6d1e7e197575dbff85348d6d973cc4c8b6279
fahimahmadi <a.fahimahmadi@gmail.com>
2019-11-06 02:40:12 +0100

    initial commit

---
+++ src/components/contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Contact extends Component {
+  render() {
+    return (
+      <div className="container bg-light pt-5 d-flex justify-content-center">
+        <h3>This is contact</h3>
+      </div>
+    );
+  }
+}
+
+export default Contact;
