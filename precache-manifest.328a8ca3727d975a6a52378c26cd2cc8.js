7ee90be6376f8e25f31aa7a311e9ad83560d3bf2
fahimahmadi <a.fahimahmadi@gmail.com>
2019-11-06 02:39:40 +0100

    Updates

---
+++ precache-manifest.328a8ca3727d975a6a52378c26cd2cc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea33668641726043eafb99622e7f503",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "423a456eb6e0f4f2d723",
+    "url": "/todo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "75019c96e4dbc4933c8e",
+    "url": "/todo/static/js/2.65a6a493.chunk.js"
+  },
+  {
+    "revision": "423a456eb6e0f4f2d723",
+    "url": "/todo/static/js/main.e8190555.chunk.js"
+  },
+  {
+    "revision": "0dfeb895ffce801d1a97",
+    "url": "/todo/static/js/runtime-main.f2a5c796.js"
+  }
+]);
